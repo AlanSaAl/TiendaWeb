a488eb78667b6e662abbb43ec5f203a47be8a2db
geraCasillas <angel.casillasps@gmail.com>
2019-11-07 19:08:32 -0600

    Index 1.1

--- index.php
+++ index.php
@@ -1,53 +1,54 @@
 <?php include("includes/header.php") ?>
+<hr>
 
 <body style="background: black;">
 
     <!--Slider-->
-    <div class="container-fluid">
-        <div class="Slider">
-            <div id="carousel" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel" data-slide-to="1"></li>
-                    <li data-target="#carousel" data-slide-to="2"></li>
-                    <li data-target="#carousel" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="assets/slider4.jpg" width="600" height="850" class="d-block w-100" alt="">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Diseños originales</h5>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/slider2.jpg" width="600" height="850" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Taxco De Alarcón Guerrero</h5>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/slider3.jpg" width="600" height="850" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>SilverTaxco is an online silver store, 100% Mexican. We have a wide range of high quality products, we provide a simple shopping experience, fast delivery times and the best customer service.</h5>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/slider1.jpg" width="600" height="850" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
 
-                        </div>
+    <div class="Slider">
+        <div id="carousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel" data-slide-to="1"></li>
+                <li data-target="#carousel" data-slide-to="2"></li>
+                <li data-target="#carousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="assets/slider4.jpg" width="600" height="640" class="d-block w-100" alt="">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Diseños originales</h5>
                     </div>
                 </div>
-                <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <div class="carousel-item">
+                    <img src="assets/slider2.jpg" width="600" height="640" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Taxco De Alarcón Guerrero</h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/slider3.jpg" width="600" height="640" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>SilverTaxco is an online silver store, 100% Mexican. We have a wide range of high quality products, we provide a simple shopping experience, fast delivery times and the best customer service.</h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/slider1.jpg" width="600" height="850" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>SilverTaxco</h5>
+                    </div>
+                </div>
             </div>
+            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
+
     </div>
     <!--Fin Slider-->
 
@@ -55,11 +56,14 @@
     <!--Carrusel-->
 
     <!--Texto plata .95-->
-    <div class="container" style="">
-        <div class="row">
-            <div></div>
 
-        </div>
-    </div>
+    <section class="p-3 mb-2 bg-white text-dark">
+
+        <p class="mt-4 pt-3 text-center">Plata .925</p>
+
+        <p class="mt-2 pt-3 text-center"> La plata 925 es un símbolo que utilizan los artesanos para determinar el grado de pureza incluido en una pieza. Ofrecemos 100% de plata mexicana de Taxco de Alarcón con .925, 92.5% de plata pura y 7.5% de cobre, asegurando su pureza y durabilidad.</p>
+    </section>
+
+    
 
     <?php include("includes/footer.php") ?>
